fbe9067a675791f6e7e5a4aefcbd1c9ef85662bf
lubianat <tiago.lubiana.alves@usp.br>
2020-03-16 20:29:22 -0300

    ace correlation for helder

---
+++ pubscore/src/ace_correlation.R
@@ -0,0 +1,18 @@
+library("data.table")
+library("PubScore")
+
+
+genes <- fread("pubscore/data/correlated genes.txt", header = F)
+
+genes <- genes$V1
+
+
+pubscore_object <- pubscore(terms_of_interest = "ACE2", genes = genes)
+
+
+p <- heatmapViz(pubscore_object)
+library(plotly)
+py <- ggplotly(p)
+p
+htmlwidgets::saveWidget(py, "index.html")
+
