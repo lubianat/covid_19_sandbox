0c098d895a054a633a6d55506d99100ad0aaece7
lubianat <tiago.lubiana.alves@usp.br>
2020-03-28 14:04:22 -0300

    recovering bits of code that were deleted previously

--- who_to_wikidata/functions.py
+++ who_to_wikidata/functions.py
@@ -86,3 +86,31 @@ def get_wikidata_date_from_report_number(report_number):
     report_date = first_report_date + timedelta(report_number)
     date_string = report_date.strftime("+%Y-%m-%dT00:00:00Z/11")
     return(date_string)
+
+
+import pandas as pd
+
+def match_who_table_to_qids(who, dictionary, output_file):
+    who_table_with_qids_by_label = who.merge(dictionary, on = "country", how = "left" )
+    who_table_with_qids_by_label = who_table_with_qids_by_label[["country", "total_cases", "total_deaths", "outbreak_qid", "outbreak"]].dropna().drop_duplicates()
+
+    who_table_with_qids_by_alias = who.merge(dictionary, left_on = "country", right_on = "other_names", how = "left" )
+    who_table_with_qids_by_alias = who_table_with_qids_by_alias[["country_x", "total_cases", "total_deaths", "outbreak_qid", "outbreak"]].dropna().drop_duplicates()
+    who_table_with_qids_by_alias.columns = ["country", "total_cases", "total_deaths", "outbreak_qid", "outbreak"]
+
+
+    ## Now we will join both tables 
+    who_table_with_qids = who_table_with_qids_by_label.append(who_table_with_qids_by_alias)
+
+    #t will have to be manually curated for items that possibly are mistaken. It is okay.
+    who_table_with_qids.to_csv(output_file)
+    
+    return(who_table_with_qids)
+
+def list_duplicates(seq):
+  seen = set()
+  seen_add = seen.add
+  # adds all elements it doesn't know yet to seen and all other to seen_twice
+  seen_twice = set( x for x in seq if x in seen or seen_add(x) )
+  # turn the set into a list (as requested)
+  return list( seen_twice )
