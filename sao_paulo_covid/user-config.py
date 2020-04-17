423bdce0a773356b0f8bf3e1998d3ce06d693fec
lubianat <tiago.lubiana.alves@usp.br>
2020-04-17 09:38:56 -0300

    trying and failing to set pywikibot

---
+++ sao_paulo_covid/user-config.py
@@ -0,0 +1,10 @@
+mylang = 'commons'
+family = 'commons'
+usernames['commons']['commons'] = u'TiagoLubiana'
+
+
+
+usernames['wikipedia']['en'] = 'TiagoLubiana'
+
+console_encoding = 'utf-8'
+use_api_login = True
